e9737622e697b278681d07e1000cba715e58a89c
Jeruzael <dumale.jeruzael.bscs2019@gmail.com>
2026-01-08 11:52:35 +0800

    master

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,7 @@ LOGGING = {
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", ""),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
 }
